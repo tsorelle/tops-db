e84d76bc3805b3d0b5ae1b72e6d740a828cf9fd2
Terry SoRelle <tls@2quakers.net>
2017-08-11 14:51:20 -0500

    changes to test database

--- tests/sql/test-db.sql
+++ tests/sql/test-db.sql
@@ -19,25 +19,25 @@ USE `twoquake_test`;
 DROP TABLE IF EXISTS `bookstore_customers`;
 
 CREATE TABLE `bookstore_customers` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `customertypeid` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `address` varchar(50) DEFAULT NULL,
-  `city` varchar(50) DEFAULT NULL,
-  `state` varchar(50) DEFAULT NULL,
-  `postalcode` varchar(50) DEFAULT NULL,
-  `buyer` varchar(50) DEFAULT NULL,
-  `createdby` varchar(50) DEFAULT NULL,
-  `createdon` datetime DEFAULT NULL,
-  `changedby` varchar(50) DEFAULT NULL,
-  `changedon` datetime DEFAULT NULL,
-  `active` tinyint(1) NOT NULL DEFAULT '1',
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `customertypeid` INT(11) NOT NULL,
+  `name` VARCHAR(50) NOT NULL,
+  `address` VARCHAR(50) DEFAULT NULL,
+  `city` VARCHAR(50) DEFAULT NULL,
+  `state` VARCHAR(50) DEFAULT NULL,
+  `postalcode` VARCHAR(50) DEFAULT NULL,
+  `buyer` VARCHAR(50) DEFAULT NULL,
+  `createdby` VARCHAR(50) DEFAULT NULL,
+  `createdon` DATETIME DEFAULT NULL,
+  `changedby` VARCHAR(50) DEFAULT NULL,
+  `changedon` DATETIME DEFAULT NULL,
+  `active` TINYINT(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=MYISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 /*Data for the table `bookstore_customers` */
 
-insert  into `bookstore_customers`(`id`,`customertypeid`,`name`,`address`,`city`,`state`,`postalcode`,`buyer`,`createdby`,`createdon`,`changedby`,`changedon`,`active`) values 
+INSERT  INTO `bookstore_customers`(`id`,`customertypeid`,`name`,`address`,`city`,`state`,`postalcode`,`buyer`,`createdby`,`createdon`,`changedby`,`changedon`,`active`) VALUES 
 (1,2,'Kids Korner Bookstore','3001 Bee Caves Road','Austin','TX','78746','bob','admin','2017-05-06 06:42:26','admin','2017-05-06 06:42:26',1),
 (2,1,'Kinder Kindles','9032 Main','Boston','MA','02746','','admin','2017-05-06 06:42:26','admin','2017-05-06 06:42:26',1);
 
@@ -46,19 +46,19 @@ insert  into `bookstore_customers`(`id`,`customertypeid`,`name`,`address`,`city`
 DROP TABLE IF EXISTS `bookstore_customertypes`;
 
 CREATE TABLE `bookstore_customertypes` (
-  `id` int(11) NOT NULL,
-  `typedescription` varchar(50) NOT NULL,
-  `discount` int(11) NOT NULL,
-  `createdby` varchar(50) DEFAULT NULL,
-  `createdon` datetime DEFAULT NULL,
-  `changedby` varchar(50) DEFAULT NULL,
-  `changedon` datetime DEFAULT NULL,
+  `id` INT(11) NOT NULL,
+  `typedescription` VARCHAR(50) NOT NULL,
+  `discount` INT(11) NOT NULL,
+  `createdby` VARCHAR(50) DEFAULT NULL,
+  `createdon` DATETIME DEFAULT NULL,
+  `changedby` VARCHAR(50) DEFAULT NULL,
+  `changedon` DATETIME DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MYISAM DEFAULT CHARSET=latin1;
 
 /*Data for the table `bookstore_customertypes` */
 
-insert  into `bookstore_customertypes`(`id`,`typedescription`,`discount`,`createdby`,`createdon`,`changedby`,`changedon`) values 
+INSERT  INTO `bookstore_customertypes`(`id`,`typedescription`,`discount`,`createdby`,`createdon`,`changedby`,`changedon`) VALUES 
 (1,'Silver',10,'admin','2017-05-06 06:42:26','admin','2017-05-06 06:42:26'),
 (2,'Gold',20,'admin','2017-05-06 06:42:26','admin','2017-05-06 06:42:26'),
 (3,'Platinum',40,'admin','2017-05-06 06:42:26','admin','2017-05-06 06:42:26');
@@ -68,21 +68,21 @@ insert  into `bookstore_customertypes`(`id`,`typedescription`,`discount`,`create
 DROP TABLE IF EXISTS `bookstore_examples`;
 
 CREATE TABLE `bookstore_examples` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `isbn` varchar(20) NOT NULL,
-  `authors` varchar(150) DEFAULT NULL,
-  `createdby` varchar(50) DEFAULT NULL,
-  `createdon` datetime DEFAULT NULL,
-  `changedby` varchar(50) DEFAULT NULL,
-  `changedon` datetime DEFAULT NULL,
-  `price` decimal(10,2) DEFAULT '2.00',
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50) NOT NULL,
+  `isbn` VARCHAR(20) NOT NULL,
+  `authors` VARCHAR(150) DEFAULT NULL,
+  `createdby` VARCHAR(50) DEFAULT NULL,
+  `createdon` DATETIME DEFAULT NULL,
+  `changedby` VARCHAR(50) DEFAULT NULL,
+  `changedon` DATETIME DEFAULT NULL,
+  `price` DECIMAL(10,2) DEFAULT '2.00',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
+) ENGINE=MYISAM AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
 
 /*Data for the table `bookstore_examples` */
 
-insert  into `bookstore_examples`(`id`,`name`,`isbn`,`authors`,`createdby`,`createdon`,`changedby`,`changedon`,`price`) values 
+INSERT  INTO `bookstore_examples`(`id`,`name`,`isbn`,`authors`,`createdby`,`createdon`,`changedby`,`changedon`,`price`) VALUES 
 (1,'Mister Peanut Goes to Washington','99921-58-10-7','D.C. Lawrence','admin','2017-05-06 06:42:26','admin','2017-05-06 06:42:26',2.00),
 (2,'An Elephant\'s Tale','9971-5-0210-0','Parker Packiderm','admin','2017-05-06 06:42:26','admin','2017-05-06 06:42:26',2.00),
 (3,'Conversations with Babar','960-425-059-0','Jane Goodall','admin','2017-05-06 06:42:26','admin','2017-05-06 06:42:26',2.00),
@@ -94,18 +94,18 @@ insert  into `bookstore_examples`(`id`,`name`,`isbn`,`authors`,`createdby`,`crea
 DROP TABLE IF EXISTS `bookstore_invoicelineitems`;
 
 CREATE TABLE `bookstore_invoicelineitems` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `invoiceid` int(11) NOT NULL,
-  `titleid` int(11) NOT NULL,
-  `supplierid` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `cost` decimal(10,0) NOT NULL,
-  `createdby` varchar(50) DEFAULT NULL,
-  `createdon` datetime DEFAULT NULL,
-  `changedby` varchar(50) DEFAULT NULL,
-  `changedon` datetime DEFAULT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `invoiceid` INT(11) NOT NULL,
+  `titleid` INT(11) NOT NULL,
+  `supplierid` INT(11) NOT NULL,
+  `quantity` INT(11) NOT NULL,
+  `cost` DECIMAL(10,0) NOT NULL,
+  `createdby` VARCHAR(50) DEFAULT NULL,
+  `createdon` DATETIME DEFAULT NULL,
+  `changedby` VARCHAR(50) DEFAULT NULL,
+  `changedon` DATETIME DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MYISAM DEFAULT CHARSET=latin1;
 
 /*Data for the table `bookstore_invoicelineitems` */
 
@@ -114,16 +114,16 @@ CREATE TABLE `bookstore_invoicelineitems` (
 DROP TABLE IF EXISTS `bookstore_invoices`;
 
 CREATE TABLE `bookstore_invoices` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `customerid` int(11) NOT NULL,
-  `status` int(11) NOT NULL,
-  `invoicedate` datetime NOT NULL,
-  `createdby` varchar(50) DEFAULT NULL,
-  `createdon` datetime DEFAULT NULL,
-  `changedby` varchar(50) DEFAULT NULL,
-  `changedon` datetime DEFAULT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `customerid` INT(11) NOT NULL,
+  `status` INT(11) NOT NULL,
+  `invoicedate` DATETIME NOT NULL,
+  `createdby` VARCHAR(50) DEFAULT NULL,
+  `createdon` DATETIME DEFAULT NULL,
+  `changedby` VARCHAR(50) DEFAULT NULL,
+  `changedon` DATETIME DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MYISAM DEFAULT CHARSET=latin1;
 
 /*Data for the table `bookstore_invoices` */
 
@@ -132,14 +132,14 @@ CREATE TABLE `bookstore_invoices` (
 DROP TABLE IF EXISTS `bookstore_orderstatuses`;
 
 CREATE TABLE `bookstore_orderstatuses` (
-  `id` int(11) NOT NULL,
-  `status` varchar(50) NOT NULL,
+  `id` INT(11) NOT NULL,
+  `status` VARCHAR(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MYISAM DEFAULT CHARSET=latin1;
 
 /*Data for the table `bookstore_orderstatuses` */
 
-insert  into `bookstore_orderstatuses`(`id`,`status`) values 
+INSERT  INTO `bookstore_orderstatuses`(`id`,`status`) VALUES 
 (0,'New'),
 (1,'Submitted'),
 (2,'Fullfilled'),
@@ -150,47 +150,47 @@ insert  into `bookstore_orderstatuses`(`id`,`status`) values
 DROP TABLE IF EXISTS `bookstore_suppliers`;
 
 CREATE TABLE `bookstore_suppliers` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT NULL,
-  `address` varchar(50) DEFAULT NULL,
-  `city` varchar(50) DEFAULT NULL,
-  `state` varchar(50) DEFAULT NULL,
-  `postalcode` varchar(50) DEFAULT NULL,
-  `createdby` varchar(50) DEFAULT NULL,
-  `createdon` datetime DEFAULT NULL,
-  `changedby` varchar(50) DEFAULT NULL,
-  `changedon` datetime DEFAULT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50) DEFAULT NULL,
+  `address` VARCHAR(50) DEFAULT NULL,
+  `city` VARCHAR(50) DEFAULT NULL,
+  `state` VARCHAR(50) DEFAULT NULL,
+  `postalcode` VARCHAR(50) DEFAULT NULL,
+  `createdby` VARCHAR(50) DEFAULT NULL,
+  `createdon` DATETIME DEFAULT NULL,
+  `changedby` VARCHAR(50) DEFAULT NULL,
+  `changedon` DATETIME DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=MYISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 /*Data for the table `bookstore_suppliers` */
 
-insert  into `bookstore_suppliers`(`id`,`name`,`address`,`city`,`state`,`postalcode`,`createdby`,`createdon`,`changedby`,`changedon`) values 
+INSERT  INTO `bookstore_suppliers`(`id`,`name`,`address`,`city`,`state`,`postalcode`,`createdby`,`createdon`,`changedby`,`changedon`) VALUES 
 (1,'Wholesale Books','2342 Main','New York','New York','34523','admin','2017-05-06 06:49:55','admin','2017-05-06 06:49:55'),
 (2,'Kidz Bookz R Uz','909 N. Lamar','Austin','Texas','78767','admin','2017-05-06 06:51:53','admin','2017-05-06 06:51:53'),
 (3,'Amazon.com','','','','','admin','2017-05-06 06:51:53','admin','2017-05-06 06:51:53');
 
-/*Table structure for table `bookstore_suppliertitles` */
+/*Table structure for table `bookstore_supplier_titles` */
 
-DROP TABLE IF EXISTS `bookstore_suppliertitles`;
+DROP TABLE IF EXISTS `bookstore_supplier_titles`;
 
-CREATE TABLE `bookstore_suppliertitles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `supplierid` int(11) NOT NULL,
-  `titleid` int(11) NOT NULL,
-  `discountquantity` int(11) NOT NULL,
-  `unitprice` decimal(10,0) NOT NULL,
-  `effectivedate` datetime NOT NULL,
-  `createdby` varchar(50) DEFAULT NULL,
-  `createdon` datetime DEFAULT NULL,
-  `changedby` varchar(50) DEFAULT NULL,
-  `changedon` datetime DEFAULT NULL,
+CREATE TABLE `bookstore_supplier_titles` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `supplierid` INT(11) NOT NULL,
+  `titleid` INT(11) NOT NULL,
+  `discountquantity` INT(11) NOT NULL,
+  `unitprice` DECIMAL(10,0) NOT NULL,
+  `effectivedate` DATETIME NOT NULL,
+  `createdby` VARCHAR(50) DEFAULT NULL,
+  `createdon` DATETIME DEFAULT NULL,
+  `changedby` VARCHAR(50) DEFAULT NULL,
+  `changedon` DATETIME DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+) ENGINE=MYISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
-/*Data for the table `bookstore_suppliertitles` */
+/*Data for the table `bookstore_supplier_titles` */
 
-insert  into `bookstore_suppliertitles`(`id`,`supplierid`,`titleid`,`discountquantity`,`unitprice`,`effectivedate`,`createdby`,`createdon`,`changedby`,`changedon`) values 
+INSERT  INTO `bookstore_supplier_titles`(`id`,`supplierid`,`titleid`,`discountquantity`,`unitprice`,`effectivedate`,`createdby`,`createdon`,`changedby`,`changedon`) VALUES 
 (1,1,1,10,22,'2017-05-05 06:51:53','admin','2017-05-06 06:51:53','admin','2017-05-06 06:51:53'),
 (2,1,2,15,5,'2017-05-05 06:51:53','admin','2017-05-06 06:51:53','admin','2017-05-06 06:51:53'),
 (3,1,3,10,15,'2017-05-05 06:51:53','admin','2017-05-06 06:51:53','admin','2017-05-06 06:51:53'),
@@ -208,9 +208,9 @@ insert  into `bookstore_suppliertitles`(`id`,`supplierid`,`titleid`,`discountqua
 DROP TABLE IF EXISTS `bookstore_testtable`;
 
 CREATE TABLE `bookstore_testtable` (
-  `id` int(11) NOT NULL,
+  `id` INT(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MYISAM DEFAULT CHARSET=latin1;
 
 /*Data for the table `bookstore_testtable` */
 
@@ -219,20 +219,20 @@ CREATE TABLE `bookstore_testtable` (
 DROP TABLE IF EXISTS `bookstore_titles`;
 
 CREATE TABLE `bookstore_titles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `isbn` varchar(20) NOT NULL,
-  `authors` varchar(150) DEFAULT NULL,
-  `createdby` varchar(50) DEFAULT NULL,
-  `createdon` datetime DEFAULT NULL,
-  `changedby` varchar(50) DEFAULT NULL,
-  `changedon` datetime DEFAULT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50) NOT NULL,
+  `isbn` VARCHAR(20) NOT NULL,
+  `authors` VARCHAR(150) DEFAULT NULL,
+  `createdby` VARCHAR(50) DEFAULT NULL,
+  `createdon` DATETIME DEFAULT NULL,
+  `changedby` VARCHAR(50) DEFAULT NULL,
+  `changedon` DATETIME DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=MYISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 /*Data for the table `bookstore_titles` */
 
-insert  into `bookstore_titles`(`id`,`name`,`isbn`,`authors`,`createdby`,`createdon`,`changedby`,`changedon`) values 
+INSERT  INTO `bookstore_titles`(`id`,`name`,`isbn`,`authors`,`createdby`,`createdon`,`changedby`,`changedon`) VALUES 
 (1,'Mister Peanut Goes to Washington','99921-58-10-7','D.C. Lawrence','admin','2017-05-06 06:42:26','admin','2017-05-06 06:42:26'),
 (2,'An Elephant\'s Tale','9971-5-0210-0','Parker Packiderm','admin','2017-05-06 06:42:26','admin','2017-05-06 06:42:26'),
 (3,'Conversations with Babar','960-425-059-0','Jane Goodall','admin','2017-05-06 06:42:26','admin','2017-05-06 06:42:26'),
