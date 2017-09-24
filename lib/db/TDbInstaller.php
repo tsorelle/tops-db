996b93fde10192e9337e6a5b8fdfcccdecb4b535
Terry SoRelle <tls@2quakers.net>
2017-09-24 13:23:04 -0500

    updated TDbInstaller to support uninstall and testing

--- lib/db/TDbInstaller.php
+++ lib/db/TDbInstaller.php
@@ -9,7 +9,6 @@
 namespace Tops\db;
 
 
-use Tops\sys\TConfiguration;
 use Tops\sys\TIniSettings;
 use Tops\sys\TPath;
 
@@ -17,12 +16,12 @@ class TDbInstaller
 {
     private $log = array();
 
-    public function installSchema(TIniSettings $config, $scriptPath='application/install') {
-        $databaseName = $config->getValue('database','settings',null);
-        $connection = TDatabase::getConnection($databaseName);
+    public function testInstallSchema(TIniSettings $config, $scriptPath = 'application/install') {
         $tables = $config->getSection('tables');
         foreach ($tables as $tableName => $expectedCount) {
-            $this->createTable($tableName,$expectedCount, $scriptPath, $connection);
+            $script = TPath::fromFileRoot($scriptPath) . '/' . "create-table-$tableName.sql";
+            $message = file_exists($script) ? "TEST: Created table: $tableName" : "Create script for $tableName not found.";
+            $this->writeLog($message);
         }
         if (empty($this->log)) {
             $this->log[] = 'Schema: no changes needed.';
@@ -30,23 +29,61 @@ class TDbInstaller
         return $this->log;
     }
 
-    protected function createTable($tableName,$expectedRowCount, $scriptPath,$connection) {
+    public function installSchema(TIniSettings $config, $scriptPath = 'application/install')
+    {
+        $databaseName = $config->getValue('database', 'settings', null);
+        $connection = TDatabase::getConnection($databaseName);
+        $tables = $config->getSection('tables');
+        foreach ($tables as $tableName => $expectedCount) {
+            $this->createTable($tableName, $expectedCount, $scriptPath, $connection);
+        }
+        if (empty($this->log)) {
+            $this->log[] = 'Schema: no changes needed.';
+        }
+        return $this->log;
+    }
+
+    public function dropSchema(TIniSettings $config, $testing=false)
+    {
+        $databaseName = $config->getValue('database', 'settings', null);
+        $connection = TDatabase::getConnection($databaseName);
+        $tables = array_reverse($config->getSection('tables'));
+        foreach ($tables as $tableName => $expectedCount) {
+            if ($testing) {
+                $this->log[] = "TEST: Would have dropped table: $tableName";
+            }
+            else {
+                $query = $connection->prepare("DROP TABLE $tableName");
+                $query->execute();
+                $this->log[] = "Dropped table: $tableName";
+            }
+        }
+        if (empty($this->log)) {
+            $this->log[] = 'Schema: no changes needed.';
+        }
+        return $this->log;
+    }
+
+    protected function createTable($tableName, $expectedRowCount, $scriptPath, $connection)
+    {
         $result = TDatabase::rowCount($tableName);
         if ($result === false || $result < $expectedRowCount) {
-            $token=\Tops\sys\TSession::GetSecurityToken();
-            $script = TPath::fromFileRoot($scriptPath).'/'."create-table-$tableName.sql";
-            TDatabase::ExecuteSql($token,$script,$connection);
+            $token = \Tops\sys\TSession::GetSecurityToken();
+            $script = TPath::fromFileRoot($scriptPath) . '/' . "create-table-$tableName.sql";
+            TDatabase::ExecuteSql($token, $script, $connection);
             $this->writeLog("Created table: $tableName");
             return 0;
         }
         return $result;
     }
 
-    protected function clearLog() {
+    protected function clearLog()
+    {
         $this->log = array();
     }
 
-    protected function writeLog($message) {
+    protected function writeLog($message)
+    {
         if (!empty($message)) {
             $this->log[] = $message;
         }
