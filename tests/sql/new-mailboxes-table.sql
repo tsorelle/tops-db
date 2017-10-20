f97331e6b4e9f82569d760caee62a8d44d4ccb1e
Terry SoRelle <tls@2quakers.net>
2017-10-20 11:55:21 -0500

    corrected test sql for create mailboxes

--- tests/sql/new-mailboxes-table.sql
+++ tests/sql/new-mailboxes-table.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `tops_mailboxes` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `mailboxcode` VARCHAR(30) NOT NULL DEFAULT '',
   `address` VARCHAR(100) DEFAULT NULL,
-  `displayText` VARCHAR(100) DEFAULT NULL,
+  `displaytext` VARCHAR(100) DEFAULT NULL,
   `description` VARCHAR(100) DEFAULT NULL,
   `createdby` VARCHAR(50) NOT NULL DEFAULT 'unknown',
   `createdon` DATETIME DEFAULT CURRENT_TIMESTAMP,
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS `tops_mailboxes` (
 
 DELETE FROM tops_mailboxes;
 
-INSERT  INTO `tops_mailboxes`(mailboxcode,`address`,`displayText`,`active`,`public`) VALUES
+INSERT  INTO `tops_mailboxes`(mailboxcode,`address`,`displaytext`,`active`,`public`) VALUES
   ('clerk','clerk@friends.com','Clerk',1,1),
   ('support','support@friends.com','Support',1,1);
 
